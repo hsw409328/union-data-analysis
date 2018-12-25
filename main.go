9da7aa8e72e646e599b5bb87ca325e733eafeccf
hsw409328 <409328820@qq.com>
2018-12-25 23:18:15 +0800

    fix； comment cgtime lastime

--- main.go
+++ main.go
@@ -76,10 +76,10 @@ func add(lastDate string, lastStartTime string, lastEndTime string) {
 
 	// 计算每天的所有订单业绩
 	orderDataSlice := model.NewOrder().Where(map[string]string{
-		//"cgtime": " between '" + lastStartTime + "' and '" + lastEndTime + "' ",
+		"cgtime": " between '" + lastStartTime + "' and '" + lastEndTime + "' ",
 		//"cgtime": " between '2018-11-01 00:00:00' and '2018-12-01 23:59:59' ",
-		"cgtime": " between '2018-01-01 00:00:00' and '" + lastEndTime + "'  ",
-		"订单状态":   " = '订单结算'",
+		//"cgtime": " between '2018-01-01 00:00:00' and '" + lastEndTime + "'  ",
+		"订单状态": " = '订单结算'",
 	}).Group("ID").GetAll()
 	//每笔订单的业绩计算方法：
 	//业绩=自己的佣金 - 上级佣金 - 一级佣金 -（联盟佣金*12%）
@@ -150,10 +150,10 @@ func reduce(lastDate string, lastStartTime string, lastEndTime string) {
 
 	// 计算每天的所有订单业绩
 	orderDataSlice := model.NewOrder().Where(map[string]string{
-		//"cgtime": " between '" + lastStartTime + "' and '" + lastEndTime + "' ",
+		"cgtime": " between '" + lastStartTime + "' and '" + lastEndTime + "' ",
 		//"cgtime": " between '2018-11-01 00:00:00' and '2018-12-01 23:59:59' ",
-		"cgtime": " between '2018-01-01 00:00:00' and '" + lastEndTime + "' ",
-		"订单状态":   " = '失效订单'",
+		//"cgtime": " between '2018-01-01 00:00:00' and '" + lastEndTime + "' ",
+		"订单状态": " = '失效订单'",
 	}).Group("ID").GetAll()
 	//每笔订单的业绩计算方法：
 	//业绩=自己的佣金 -（联盟佣金*12%）
