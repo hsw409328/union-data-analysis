5f35ffb5f33739b56beb2da1a841dce49c2a9744
haoshuaiwei <haoshuaiwei@jd.com>
2019-01-19 12:41:03 +0800

    feat: 添加支付页面

--- model/base.go
+++ model/base.go
@@ -11,7 +11,7 @@ const (
 	ProxyTableName = "代理管理"
 
 	WebUserTableName       = "用户表"
-	WebPayOrderTableName       = "用户支付表"
+	WebPayOrderTableName   = "用户支付表"
 	WebDayRecordTableName  = "每日记录表"
 	WebSendRecordTableName = "发放记录表"
 	WebRecommendTableName  = "一级推荐表"
